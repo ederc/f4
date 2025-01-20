81aa80954774a4d8c14f921ffd2ef1350d67f603
ederc <ederc@mathematik.uni-kl.de>
2025-01-20 12:40:07 +0100

    implements initialization of basis struct

--- src/basis.rs
+++ src/basis.rs
@@ -42,6 +42,7 @@ impl<'a> Basis<'a> {
             elements             : Vec::new(),
         };
         basis.add_initial_elements(coefficients, exponents);
+        basis.sort_terms_by_drl();
         basis.sort_elements_by_drl();
         return basis;
     }
@@ -57,11 +58,37 @@ impl<'a> Basis<'a> {
                 monomials: e.iter().map(|a| hash_table.insert(a)).collect(),
                 is_redundant: false});
         }
+
+    }
+    fn sort_terms_by_drl(&mut self) {
+        for e in &mut self.elements {
+            let mut zipped: Vec<_> = e.monomials.clone().into_iter().zip(e.coefficients.clone()).collect();
+            zipped.sort_unstable_by(|(a, _), (b, _)| self.hash_table
+                 .cmp_monomials_by_drl(*a, *b));
+            (e.monomials, e.coefficients) = zipped.into_iter().unzip();
+        }
     }
 
     fn sort_elements_by_drl(&mut self) {
         return self.elements.sort_by(
             |a,b| self.hash_table.cmp_monomials_by_drl(
                 a.monomials[0], b.monomials[0]));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_initialize_basis() {
+        let fc : Characteristic = 65521;
+        let cfs : Vec<CoeffVec> = vec![vec![-2,65523], vec![1, -3]];
+        let exps : Vec<Vec<ExpVec>> = vec![vec![vec![0,3], vec![1,1]], vec![vec![0,2], vec![1,1]]];
+        let basis = Basis::new::<i32>(fc, &cfs, &exps);
+        assert_eq!(basis.elements[0].coefficients, [1,-3]);
+        assert_eq!(basis.elements[0].monomials, [2,1]);
+        assert_eq!(basis.elements[1].coefficients, [2,-2]);
+        assert_eq!(basis.elements[1].monomials, [1,0]);
     }
 }
