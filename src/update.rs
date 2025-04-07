6c6af6efe0c6ec90549c79e39ca82fc25b7233c6
ederc <ederc@mathematik.uni-kl.de>
2025-04-07 11:32:49 +0200

    implements loading of lcm data vec

--- src/update.rs
+++ src/update.rs
@@ -14,37 +14,32 @@ use crate::hash_table::{
 };
 
 #[derive(Debug)]
-pub struct LcmGenerators<'a> {
+pub struct LcmData {
     lcm: HashTableLength,
-    generators: BTreeSet<&'a Element>,
+    generators: BTreeSet<BasisLength>,
 }
 
-pub type LcmVec<'a> = Vec<LcmGenerators<'a>>;
+pub type LcmVec = Vec<LcmData>;
 
 pub fn update_lcms(lcm_vec: &mut LcmVec, basis: &mut Basis, hash_table: &mut HashTable) {
 
-    while (basis.previous_length as usize) < basis.elements.len() {
-        let e = &basis.elements[basis.previous_length as usize];
-        for f in &basis.elements[0..(basis.previous_length as usize)] {
+    let mut e_idx = basis.previous_length as usize;
+    while e_idx < basis.elements.len() {
+        let e = &basis.elements[e_idx];
+        for (f_idx, f) in basis.elements[0..e_idx].iter().enumerate() {
             let lcm = hash_table.get_lcm(e.monomials[0], f.monomials[0]);
-            match lcm_vec.iter().find(|x| x.lcm == lcm) {
-                Some(olcm) => {
-                    olcm.generators.insert(&e);
-                    olcm.generators.insert(&f);
-                },
-                None => {
-                    lcm_vec.push(LcmGenerators {
-                        lcm: lcm,
-                        generators: BTreeSet::new(),
-                    }),
-                    lcm_vec[lcm_vec.len()-1].generators.insert(&e),
-                    lcm_vec[lcm_vec.len()-1].generators.insert(&f),
-                },
+            let pos = lcm_vec.iter().position(|x| x.lcm == lcm).unwrap_or(lcm_vec.len());
+            println!("pos {}", pos);
+
+            if pos == lcm_vec.len() {
+                lcm_vec.push(LcmData { lcm: lcm, generators: BTreeSet::new(), });
             }
-            println!("{}", lcm_vec.len());
+            lcm_vec[pos].generators.insert(e_idx as BasisLength);
+            lcm_vec[pos].generators.insert(f_idx as BasisLength);
+            println!("{:?}", lcm_vec[pos].generators);
             // lcm_vec[idx].generators.insert(&e);
             // lcm_vec[idx].generators.insert(&f);
         }
-        basis.previous_length += 1;
+        e_idx += 1;
     }
 }
